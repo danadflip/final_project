4319938874ccd3395f2f35216f0218c8d33dac6c
Dana DeFilippo <dana.dflip@gmail.com>
2015-03-09 03:35:52 -0400

    adds gumby and fixes last temp timing

--- server.rb
+++ server.rb
@@ -12,7 +12,6 @@ module Drive
 
 
     get("/") do
-
       render(:erb, :index, { :layout => :default_layout })
     end
 
@@ -23,25 +22,55 @@ module Drive
       first_condition = first_response_by_city["hourly_forecast"][0]["condition"]
       first_temp = first_response_by_city["hourly_forecast"][0]["temp"]
       first_snow = first_response_by_city["hourly_forecast"][0]["snow"]
+      first_rain = first_response_by_city["hourly_forecast"][0]["qpf"]
+      first_percent_of_percip = first_response_by_city["hourly_forecast"][0]["pop"]
+
+
+# include an if statement that says if duration of a step is >25 miles, give a weather report
+
+
+
+
 
       # last location
       last_response_by_city = HTTParty.get("http://api.wunderground.com/api/d9ccd086534d5989/hourly/q/#{params["endLat"]},#{params["endLong"]}.json")
+      # actual hour that arrive at destination
+      duration_before_convert = params["durationOfTripInSeconds"].to_i
+      duration_converted = duration_before_convert/3600
+        # conditional statement necessary because api only gives 35 hour forecast, anything above will return nil
+        if duration_converted>35
+          duration_converted=35
+        end
+
+      # binding.pry
+
+
       # weather conditions:
-      last_condition = last_response_by_city["hourly_forecast"][0]["condition"]
-      last_temp = last_response_by_city["hourly_forecast"][0]["temp"]
-      last_snow = last_response_by_city["hourly_forecast"][0]["snow"]
-
-    params["durationOfTripInSeconds"]
-
-
+      last_condition = last_response_by_city["hourly_forecast"][duration_converted]["condition"]
+      last_temp = last_response_by_city["hourly_forecast"][duration_converted]["temp"]
+      last_snow = last_response_by_city["hourly_forecast"][duration_converted]["snow"]
+      last_rain = last_response_by_city["hourly_forecast"][duration_converted]["qpf"]
+      last_percent_of_percip = last_response_by_city["hourly_forecast"][duration_converted]["pop"]
 
 
 
+      # prepares variables for javascript
+      weather_report = {
+        first_condition: first_condition,
+        first_snow: first_snow,
+        first_temp: first_temp,
+        first_rain: first_rain,
+        first_percent_of_percip: first_percent_of_percip,
+        last_temp: last_temp,
+        last_snow: last_snow,
+        last_condition: last_condition,
+        last_rain: last_rain,
+        last_percent_of_percip: last_percent_of_percip
+      }
 
-      first_weather_report = {first_condition: first_condition, first_snow: first_snow, first_temp: first_temp, last_temp: last_temp, last_snow: last_snow, last_condition: last_condition}
-      binding.pry
+
       # render(json: temp)
-      first_weather_report.to_json
+      weather_report.to_json
     end
 
 
